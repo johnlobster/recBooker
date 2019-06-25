d27c8cf6cde7589b2dd2222217f5aad13df2360e
John Webster <JohnLobster@comcast.net>
2019-06-24 17:30:48 -0700

    server.js can now reset and seed database

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
+require("dotenv").config(); // add variables in .env file to process.env
+const executeSQLFile = require("./test/functions/executeSQLFile.js");
+
 const express = require("express");
 var exphbs = require("express-handlebars");
 
-var db = require("./models");
+var db = require("./models"); // reads index.js which reads all models in models directory
 
 var app = express();
 const PORT = process.env.PORT || 8080; // note app port, not mysql
@@ -25,23 +27,49 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+// setup sequelize .sync options
+// .force will drop all tables. use for testing and, if in development and DEV_MYSQL_FORCE_DB_RESET is set
+// .logging will log SQL requests to the console. Use in test and development
+var syncOptions = { force: false, logging: false };
+var seedDB = false; // don't seed database by default
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test" || process.env.DEV_MYSQL_FORCE_DB_RESET) {
+if (
+  process.env.NODE_ENV === "test" ||
+  (process.env.NODE_ENV === "development" &&
+    process.env.DEV_MYSQL_FORCE_DB_RESET)
+) {
   syncOptions.force = true;
+  seedDB = true;
+}
+if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development") {
+  syncOptions.logging = console.log;
 }
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+// Starting the server, syncing models, seed DB if necessary (promise chain)
+db.sequelize
+  .sync(syncOptions)
+  .then(function() {
+    // seed database with known values for testing
+    return executeSQLFile(db, "./db/seed.mysql", seedDB);
+  })
+  .then(() => {
+    console.log("Now start listening");
+    return new Promise(resolve => {
+      app.listen(PORT, () => {
+        resolve();
+      });
+    });
+  })
+  .then(() => {
     console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      "==> Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
       PORT
     );
+  })
+  .catch(err => {
+    console.log("\nError in start sequelize/app listen setup\n");
+    console.log(err);
   });
-});
 
 module.exports = app;
--- test/functions/executeSQLFile.js
+++ test/functions/executeSQLFile.js
@@ -1,10 +1,11 @@
 // support function - reads a file of SQL queries and sends them to the database
 // one at a time
 // useful for executing seed file for testing/development
+// returns promise
 
 fs = require("fs");
 
-// reads file and parses into an array of lines - returns promise
+// reads file of SQL commands and parses into an array of lines - returns promise
 function readFilePromise(fileName) {
     return new Promise((resolve, reject) => {
         fs.readFile(fileName, (err, data) => {
@@ -13,8 +14,10 @@ function readFilePromise(fileName) {
                 reject(err);
             }
             else{
-                // break data down into lines, remove \r
-                resolve(data.toString().split("\n")
+                // break data down into lines, remove \r, strip out empty lines (mySQL doesn't accept)
+                resolve(data
+                .toString()
+                .split("\n")
                 .map((line) => {
                     return line.replace(/\r/g, "");
                 })
@@ -37,23 +40,35 @@ function readFilePromise(fileName) {
 //     });
 
 // read .sql file and execute array of commands in mysql database. Returns promise
-module.exports = function( dataBase, fileName) {
+module.exports = function( dataBase, fileName, run) {
     return new Promise((resolve, reject) => {
-        readFilePromise(fileName)
-        .then( (sqlCommands) => {
-            // reduce method can be used to execute promises in sequence
-            sqlCommands.reduce( (previousPromise, query) => {
-                return previousPromise.then(() => {
-                    console.log("Running query " + query);
-                    return dataBase.sequelize.query(query)
-                });
-            }, Promise.resolve());
+        if (run) {
+            readFilePromise(fileName)
+            .then( (sqlCommands) => {
+                // reduce method can be used to execute promises in sequence
+                sqlCommands
+                .reduce( (previousPromise, query) => {
+                    return previousPromise
+                    .then(() => {
+                        // console.log("QUERY = " + query);
+                        return dataBase.sequelize.query(query);
+                    });
+                    }, Promise.resolve()) // Promise.resolve is the initial value to .reduce
+                .then( () => {
+                    // console.log("All queries completed");
+                        resolve();
+                    });
+            })
+            .catch( (err) => {
+                console.log("Error in executeSQLFile");
+                console.log(err);
+                reject(err);
+            });
+        }
+        else {
+            // not running so resolve with empty options. Still a promise but no asychronous behaviour
+            // console.log("No SQL seed");
             resolve();
-        })
-        .catch( (err) => {
-            console.log("Error in executeSQLFile");
-            console.log(err);
-            reject(err);
-        });
+        }
     });
 }
