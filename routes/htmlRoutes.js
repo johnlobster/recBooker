8ca8f20c99b0bcbc39f2c42a6174c02922150d51
John Webster <JohnLobster@comcast.net>
2019-06-29 13:04:24 -0700

    pulled from master

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,21 +9,8 @@ module.exports = function(app) {
   // populate facilities reserved table
   app.get("/booking", function(req, res) {
     // using temporary data for now
-    res.render("booking", { userName: "fred", bookings: [{
-      startTime: moment.utc().subtract(10, "days").format(), 
-      endTime: moment.utc().subtract(10, "days").format(),
-      UserName : 1, 
-      FacilityId:2,
-    }, {
-      startTime: moment.utc().subtract(10, "days").format(),
-      endTime: moment.utc().subtract(10, "days").format(),
-      UserName: 1,
-      FacilityId: 2,
-    }
-  ]
-      );
-  });
-
+    res.render("booking", {userName: "Fred"});
+      
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
