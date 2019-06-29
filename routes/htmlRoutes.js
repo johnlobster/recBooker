ec97aceeb74025335d2bbf8c0d02717a0c78484a
John Webster <JohnLobster@comcast.net>
2019-06-29 14:00:56 -0700

    added latest bootstrap to main.handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,4 @@
-// don't need to import because apiRoutes has already required everything
+const db = require("../models");
 
 module.exports = function(app) {
   // Load index page
@@ -6,13 +6,26 @@ module.exports = function(app) {
     res.render("index");
   });
 
+  app.get("/login", function(req, res) {
+    res.render("login");
+  });
+
   // populate facilities reserved table
   app.get("/booking", function(req, res) {
     // using temporary data for now
-    res.render("booking", {userName: "Fred"});
-      
+    // page makes an api request to populate the table
+    // pass list of facilities so user can choose
+    console.log("Found route");
+    db.Facility.findAll({
+      attributes: ["name"]
+    }).then(function(dbFacility) {
+      console.log("queried database");
+      res.render("booking");
+    });
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.status("404").render("404");
+    res.status("404").render("404",{ badUrl: req.url });
   });
 };
