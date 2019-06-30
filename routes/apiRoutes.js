1c5cf2772740d2daa25e0a6ecaa1db8eaede94ae
John Webster <JohnLobster@comcast.net>
2019-06-30 14:39:49 -0700

    api routes doesn't send out user password on booking GET

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -47,7 +47,12 @@ module.exports = function(app) {
         startTime: { [Op.between]: [firstDate, secondDate] },
         endTime: { [Op.between]: [firstDate, secondDate] }
       },
-      include: [db.User],
+      include: [
+        {
+          model: db.User,
+          attributes: ["name", "id"]
+        }
+      ],
       order: [["startTime", "ASC"]]
     })
       .then(function(dbBookDate) {
