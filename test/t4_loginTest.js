ca3bd678fb304d24d19e87a3d5be7f1964d61afb
John Webster <JohnLobster@comcast.net>
2019-07-09 15:02:26 -0700

    t4 login/logout tests working

--- test/t4_loginTest.js
+++ test/t4_loginTest.js
@@ -27,7 +27,7 @@ describe("t4_loginTest: restart server\n", () => {
   });
 });
 
-describe("login, get, logout using cookie-session (password not checked)", () => {
+describe("login, logout using cookie-session (password not checked)", () => {
   // user "Joe Smith" is not part of seed data
   it("Login user non-existent user 'Joe Smith' - expect failure", (done) => {
     chai.request(app)
@@ -44,12 +44,15 @@ describe("login, get, logout using cookie-session (password not checked)", () =>
         expect(res.status).to.equal(200, "http response code");
         expect(res).to.be.json;
         expect(res.body).to.be.null;
+        // no session should have been created
+        expect(res).to.not.have.cookie("recBookerSession");
+        expect(res).to.not.have.cookie("recBookerSession.sig");
         done();
       });
   });
 
   // user "John Smith" is part of seed data
-  it("Login user 'John Smith'", (done) => {
+  it("Login user 'John Smith', check session cookie set", (done) => {
     chai.request(app)
     .post(`/api/login`)
     .type('form')
@@ -64,8 +67,70 @@ describe("login, get, logout using cookie-session (password not checked)", () =>
       expect(res).to.be.json;
       expect(res.body.name).to.equal("John Smith");
       expect(res.body.id).to.equal(1);
-      console.log(res.cookie);
+      expect(res).to.have.cookie("recBookerSession");
+      expect(res).to.have.cookie("recBookerSession.sig");
+      // console.log(res.headers);
+      // console.log("");
       done();
     });
   });
+  it("Login user 'Marnie Thompson', then logout", (done) => {
+    let agent = chai.request.agent(app);
+    agent.post(`/api/login`)
+      .type('form')
+      .send({
+        '_method': 'post',
+        name: "Marnie Thompson",
+        password: "1234"
+      })
+      .then( (res) => {
+        //expect(err).to.be.null;
+        expect(res.status).to.equal(200, "http response code");
+        expect(res).to.be.json;
+        expect(res.body.name).to.equal("Marnie Thompson");
+        expect(res.body.id).to.equal(2);
+        expect(res).to.have.cookie("recBookerSession");
+        expect(res).to.have.cookie("recBookerSession.sig");
+        return agent.post(`/api/logout`)
+          .type('form')
+          .send({
+            '_method': 'post',
+            userName: "Marnie Thompson",
+            userId: 2
+          })
+          .then( (res2) => {
+            // if (err) {
+            //   console.log("Error in second call of login/logout");
+            //   throw err;
+            // }
+            expect(res2.status).to.equal(200, "http response code");
+            expect(res2).to.be.json;
+            expect(res2.body.result).to.equal("Logout successful");
+            expect(res2).to.not.have.cookie("recBookerSession");
+            expect(res2).to.not.have.cookie("recBookerSession.sig");
+            agent.close();
+            done()
+          });
+        });
+      });
+  it("Logout user 'Marnie Thompson' again, should fail", (done) => {
+    chai.request(app)
+    .post(`/api/logout`)
+    .type('form')
+    .send({
+      '_method': 'post',
+      userName: "Marnie Thompson",
+      userId: 2
+    })
+    .then((res) => {
+      expect(res.status).to.equal(200, "http response code");
+      expect(res).to.be.json;
+      expect(res.body).to.be.empty;
+      expect(res).to.not.have.cookie("recBookerSession");
+      expect(res).to.not.have.cookie("recBookerSession.sig");
+        
+      done()
+      });
+  })
+      
 });
